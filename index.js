19355076a73116168ba4b65f75072dabc71e750d
Miquel Mayordomo Bofill <miquel@Miquels-MacBook-Pro.local>
2023-11-08 22:33:51 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const app = require('./src/app');
+
+require('dotenv').config();
+
+require('./src/config/db');
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT);
+
+server.on('listening', () => {
+    console.log(`Server running on port: ${PORT}`);
+
+});
+
+server.on('error', (error) => {
+    console.log(error);
+});
